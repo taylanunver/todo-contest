3518e7b50f79f8645e94c2fb68adb475ca1566c5
taylanunver <taylanunver@hotmail.com>
2022-09-26 01:59:27 +0300

    Connect to database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div className="">Hello World</div>;
+  const todayDate = new Date();
+  return (
+    <div className="h-scree w-screen flex flex-col justify-center items-center">
+      <div className="text-3xl text-center pt-16">To-Do Contest</div>
+      <div className="text-cxl text-center pt-4 pb-16">
+        {todayDate.getDate()} / {todayDate.getMonth() + 1} /{" "}
+        {todayDate.getFullYear()}
+      </div>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div>Player 1</div>
+        <div>Player 2</div>
+      </div>
+    </div>
+  );
 }
